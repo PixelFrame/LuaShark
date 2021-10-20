a501754ce24a6292c61a429b54212f44604329c8
Pixel Frame <pm421@live.com>
2021-10-20 17:36:55 +0800

    Fix typo

--- dfsc.lua
+++ dfsc.lua
@@ -8,7 +8,7 @@ local dfsc = Proto("dfsc", "Distributed File System (DFS): Referral Protocol")
 function dfsc.dissector(buffer, pinfo, tree)
     if (tcp_src() == nil) then return end
     if (smb2_cmd() == nil) then return end
-    if (ioctl_function() == nil) then return end
+    if (smb2_ioctl_function_device() == nil) then return end
     if (tcp_src()() == 445 or tcp_dst()() == 445 or tcp_src()() == 139 or tcp_dst()() == 139) then
         if(smb2_cmd()() == 11) then
             if(smb2_ioctl_function_device()() == 0x0006) then
