8e9f292f723eafbaf1950e530e26345ee4ec709e
Pixel Frame <pm421@live.com>
2021-10-23 22:29:53 +0800

    Statistic for dfsc

--- dfsc.lua
+++ dfsc.lua
@@ -2,6 +2,10 @@ local tcp_src = Field.new("tcp.srcport")
 local tcp_dst = Field.new("tcp.dstport")
 local smb2_cmd = Field.new("smb2.cmd")
 local smb2_ioctl_function_device = Field.new("smb2.ioctl.function.device")
+local dfs_out_path = Field.new("smb.dfs.referral.path")
+local dfs_out_node = Field.new("smb.dfs.referral.node")
+local dfs_out_domain = Field.new("smb.dfs.referral.domain_name")
+local dfs_out_ref_flag = Field.new("smb.dfs.referral.flags.name_list_referral")
 
 local dfsc = Proto("dfsc", "Distributed File System (DFS): Referral Protocol")
 
@@ -9,9 +13,10 @@ function dfsc.dissector(buffer, pinfo, tree)
     if (tcp_src() == nil) then return end
     if (smb2_cmd() == nil) then return end
     if (smb2_ioctl_function_device() == nil) then return end
-    if (tcp_src()() == 445 or tcp_dst()() == 445 or tcp_src()() == 139 or tcp_dst()() == 139) then
-        if(smb2_cmd()() == 11) then
-            if(smb2_ioctl_function_device()() == 0x0006) then
+    if (tcp_src()() == 445 or tcp_dst()() == 445 or tcp_src()() == 139 or
+        tcp_dst()() == 139) then
+        if (smb2_cmd()() == 11) then
+            if (smb2_ioctl_function_device()() == 0x0006) then
                 tree:add(dfsc)
                 pinfo.cols.protocol = "DFSC"
             end
@@ -20,3 +25,59 @@ function dfsc.dissector(buffer, pinfo, tree)
 end
 
 register_postdissector(dfsc)
+
+local function dfsc_menu()
+    local window = TextWindow.new("DFSC Referrals")
+    local tap = Listener.new(nil, "dfsc and smb2.flags.response == 1")
+    local statistic = {}
+
+    local function remove() tap:remove() end
+    window:set_atclose(remove)
+
+    local msg = ""
+
+    function tap.packet(pinfo, tvb)
+        local entry = {}
+        print(dfs_out_ref_flag()())
+        if (dfs_out_ref_flag()() == false) then
+            entry.path = dfs_out_path()()
+            local nodes = {dfs_out_node()}
+            local nodestrings = {}
+            for _, node in pairs(nodes) do
+                table.insert(nodestrings, node())
+            end
+            entry.nodes = nodestrings
+            statistic[pinfo.number] = entry
+        else
+            entry.path = "Domain Referrals"
+            local nodes = {dfs_out_domain()}
+            local nodestrings = {}
+            for _, node in pairs(nodes) do
+                table.insert(nodestrings, node())
+            end
+            entry.nodes = nodestrings
+            statistic[pinfo.number] = entry
+        end
+    end
+
+    function tap.draw(t)
+        window:clear()
+        for num, entry in pairs(statistic) do
+            window:append("Frame " .. num .. "\n")
+            window:append(entry.path .. "\n")
+            for i = 1, #(entry.nodes) - 1, 1 do
+                window:append("├───" .. entry.nodes[i] .. "\n")
+            end
+            window:append("└───" .. entry.nodes[#(entry.nodes)] .. "\n")
+            window:append("\n")
+        end
+    end
+
+    function tap.reset()
+        window:clear()
+        ips = {}
+    end
+    retap_packets()
+end
+
+register_menu("DFSC Referrals", dfsc_menu, MENU_STAT_UNSORTED)
