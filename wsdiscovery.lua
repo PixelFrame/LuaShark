ab14fbd07042909fd2698bb9c8089ff27cf3fccd
Pixel Frame <pm421@live.com>
2021-11-25 23:05:40 +0800

    Add WS-Discovery

---
+++ wsdiscovery.lua
@@ -0,0 +1,16 @@
+local wsdiscovery = Proto("wsd", "Web Service Discovery")
+
+function wsdiscovery.dissector(buffer, pinfo, tree)
+    tree:add(wsdiscovery,buffer())
+    
+    local soapString = buffer():string(ENC_UTF_8)
+    local action = SoapString:match("<wsa:Action>http://schemas.xmlsoap.org/ws/2005/04/discovery/(.*)</wsa:Action>")
+
+    pinfo.cols.protocol = "WS-Discovery"
+    pinfo.cols.info:set("WS-Discovery: "..action)
+    
+    Dissector.get("xml"):call(buffer,pinfo,tree)
+end
+
+udp_table = DissectorTable.get("udp.port")
+udp_table:add(3702, wsdiscovery)
